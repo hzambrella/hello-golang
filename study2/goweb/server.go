add768a3dbc687a8fc4df8969506ae93114f405a
haozhao <haozhao@localhost.localdomain>
2017-02-23 14:44:58 +0800

    cookie and session(copy level)

--- study2/goweb/server.go
+++ study2/goweb/server.go
@@ -1,7 +1,7 @@
 package main
 import(
 	"fmt"
-	//"time"
+	"time"
 	"strings"
 	"net/http"
 	"log"
@@ -13,68 +13,48 @@ import(
 var globalSession *session.Manager
 
 func init(){
-	globalSession,_=session.NewSessionManager("memory","goSessionId",60)
+	globalSession,_=session.NewSessionManager("memory","goSessionid",60)
+	fmt.Println(globalSession)
 	go globalSession.GC()
 	fmt.Println("main:init complete")
 }
 
 func  main(){
 	http.HandleFunc("/login",login)
-	http.HandleFunc("/login2",loginBySession)
 	http.HandleFunc("/",sayHello)
+	http.HandleFunc("/count",count)
 //	http.HandleFunc("/cookie",getCookie)
 	fmt.Println("listen and serve at 8080:")
 	http.ListenAndServe(":8080",nil)
 }
 
+
 func login(w http.ResponseWriter,r *http.Request){
+	sess:=globalSession.SessionStart(w,r)
+	fmt.Println("sess login:start:",sess)
 	r.ParseForm()
-	fmt.Println("method:",r.Method)
-
 	if r.Method=="GET"{
-		fmt.Fprintln(w,"hello")
 		t,err:=template.ParseFiles("template/login.tmpl")
-
 		if err!=nil{
 			log.Fatal(err)
 			return
 		}
-
-		t.Execute(w,nil)
-	}else{
-			username:=r.Form["username"]
-			password:=r.Form["password"]
-		fmt.Println(w,username," come in")
-		fmt.Fprintln(w,username,password)
-	//	expirement:=time.Now().AddDate(1,0,0)
-	//	cookie:=&http.Cookie{
-	//		Name:"testCookie",
-	//		Value:User1.username,
-	//		Expires:expirement,
-	//	}
-
-	//	http.SetCookie(w,cookie)
-	//	fmt.Println(cookie)
-	}
-	fmt.Println("200,login")
-}
-
-func loginBySession(w http.ResponseWriter,r *http.Request){
-	sess:=globalSession.SessionStart(w,r)
-	r.ParseForm()
-	if r.Method=="GET"{
-		t,_:=template.ParseFiles("login")
+		//w.Header().Set("Content-Type","text/html")
 		t.Execute(w,sess.Get("username"))
 	}else{
 		sess.Set("username",r.Form["username"])
-		http.Redirect(w,r,"/",302)
+		fmt.Println("sess login post:",sess)
+		http.Redirect(w,r,"/count",302)
 	}
+	fmt.Println(http.StatusOK,"login2")
 }
+
 func sayHello(w http.ResponseWriter, r *http.Request){
 	r.ParseForm()
-	fmt.Println(r.Method,":",r.URL.Host)
-	fmt.Println(r.URL.Path,r.URL.Scheme,r.URL.Scheme,r.URL.Query)
-	fmt.Println(r.Form["url_long"])
+	fmt.Println("method:",r.Method,"host:",r.URL.Host)
+	fmt.Println("path:",r.URL.Path,"scheme",r.URL.Scheme,"Query:",r.URL.Query)
+	fmt.Println("url_long:",r.Form["url_long"])
+	fmt.Println("......................")
 	for k,v:=range r.Form{
 		fmt.Println("key:",k)
 		fmt.Println("value:",strings.Join(v,""))
@@ -83,6 +63,43 @@ func sayHello(w http.ResponseWriter, r *http.Request){
 	fmt.Println(http.StatusOK,"sayhello")
 }
 
+
+
+func count(w http.ResponseWriter,r *http.Request){
+	sess:=globalSession.SessionStart(w,r)
+	r.ParseForm()
+	createtime:=sess.Get("createtime")
+
+	if createtime==nil{
+		sess.Set("createtime",time.Now().Unix())
+	}else if (createtime.(int64)+360)<(time.Now().Unix()){
+		globalSession.SessionDestory(w,r)
+		sess=globalSession.SessionStart(w,r)
+	}
+	ct:=sess.Get("countnum")
+
+	if ct==nil{
+		sess.Set("countnum",1)
+	}else{
+		sess.Set("countnum",(ct.(int)+1))
+	}
+
+	t,err:=template.ParseFiles("template/count.tmpl")
+
+	if err!=nil{
+		log.Fatal(err)
+		return
+	}
+
+//	w.Header().Set("content-type","text/html")
+	t.Execute(w,sess.Get("countnum"))
+	fmt.Println("sess: count",sess)
+	fmt.Println(http.StatusOK,"count")
+}
+
+func observe(w http.ResponseWriter,r *http.Request){
+
+}
 /*
 func getCookie(w http.ResponseWriter, r *http.Request ){
 	r.ParseForm()
