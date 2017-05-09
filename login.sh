395bebc48f885510242785a42ac6eef86d36c876
haozhao <haozhao@localhost.localdomain>
2017-05-09 14:59:12 +0800

    1

--- login.sh
+++ login.sh
@@ -1,4 +1,1 @@
 #!/usr/bin/expect
-echo login.sh
-expect "Username"
-send "hzambrella/r"
--- push.sh
+++ push.sh
@@ -5,8 +5,14 @@ git status
 git commit -m"1"
 git status
 set timeout 30
+expect <<!
 spawn git push origin master
-./login.sh
+echo login.sh
+expect "Username"
+send "hzambrella/r"
+expect ">"
+expect eof
+!
 
 
 
