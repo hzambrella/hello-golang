7d2cd2c9c1002acb40ad9039d9e5e9b96feee477
haozhao <haozhao@localhost.localdomain>
2017-05-19 17:05:41 +0800

    1

--- http/pt/src/cmd/client/main.go
+++ http/pt/src/cmd/client/main.go
@@ -8,16 +8,32 @@ import (
 	"time"
 )
 var(
+	// server.go
+	/*
 	 setHost string= "http://localhost"
 	 setPort string=":8080"
 	 setPath string="/test/modify"
 	 urlValues url.Values=url.Values{"key": {"test1"}, "data": {"哈哈。。。"}}
+	 */
+	 //n5d
+	//192.168.0.128:9600/api/orderback/orshop/1/goods/105
+	//setHost string= "http://192.168.0.128"
+	 //setPort string=":9600"
+//	 setPath string="/api/orderback/orshop/1/goods/105"
+	// urlValues url.Values=url.Values{"price":{"1"}}
+
+	// n5d
+	setHost string= "http://192.168.0.128"
+	 setPort string=":9600"
+	setPath string="/auth/login"
+	//setHost string= "http://192.168.0.128"
 	 AllNum int=1000
-	 thread int =10
+	 thread int =100
 )
 
 type TestPt interface{
 	TestPt(u url.Values)(time.Time)
+	TestPtGet()time.Time
 }
 
 type TestParams struct{
@@ -38,7 +54,8 @@ func main(){
 		m:=&ModifyTest{set,mtChan}
 		go func(){
 			for {
-				tm1:=m.TestPt(urlValues)
+				//tm1:=m.TestPtPost(urlValues)
+				tm1:=m.TestPtGet()
 				tm2:=time.Now()
 				tm2Subtm1:=tm2.Sub(tm1)
 				fmt.Println("time use:",tm2Subtm1)
@@ -63,7 +80,7 @@ type ModifyTest struct{
 	connTime chan time.Time
 }
 
-func (m *ModifyTest)TestPt(u url.Values)time.Time{
+func (m *ModifyTest)TestPtPost(u url.Values)time.Time{
 	murl:=m.Host+m.Port+m.Path
 	tm1:=time.Now()
 
@@ -78,5 +95,25 @@ func (m *ModifyTest)TestPt(u url.Values)time.Time{
 		panic(err)
 	}
 	fmt.Println(string(result))
+	return tm1
+}
+
+func (m *ModifyTest)TestPtGet()time.Time{
+	murl:=m.Host+m.Port+m.Path
+	tm1:=time.Now()
+
+	//fmt.Println(fmt.Sprintf("post:%surl\n data:%s\n",murl,u))
+	resp, err := http.Get(murl)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	result, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+//		panic(err)
+	}
+	if err==nil{
+	fmt.Println(string(result))
+ }
 	return tm1
 }
--- http/pt/src/cmd/client/netstat.sh
+++ http/pt/src/cmd/client/netstat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-    netstat -an|grep 8080|grep EST -c
+    netstat -an|grep 9600|grep EST -c
     sleep 4
 done
 
